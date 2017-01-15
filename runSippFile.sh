c0b5fada4ead78eaa0695fce137c690a1e26b666
Lars Sydnes <sydlar@westerdals.no>
2017-01-15 20:42:21 +0100

    La inn kommentar i runSippFile.sh

--- runSippFile.sh
+++ runSippFile.sh
@@ -7,7 +7,12 @@ echo $NAME
 
 echo "#include <$SIPPDIR/sipp.hpp>" > $NAME.cpp
 cat $SIPPDIR/utils/startOfCppFile >> $NAME.cpp
+
+# Legger inn masse semikolon, for sikkerhets skyld,
+# Prosesserer også Metode-deklarasjoner ved å gjøre
+# dem om til lambda-uttrykk.
 cat $1 | awk '{print $0 ";"} /\/\//{print ";"}' | sed 's:Metode \([a-Z]*\)(:auto \1 = [\&](:g' >> $NAME.cpp
+
 cat $SIPPDIR/utils/endOfCppFile  >> $NAME.cpp
 
 g++-5 -std=c++14 $NAME.cpp -o $NAME && ./$NAME
